1dacc97f4c31c21c342a24ebbc5df23021b47f0e
Jere Sher <jeresher@g.ucla.edu>
2020-09-01 16:57:36 -0700

    added ability to broadcast chain and replace chain via pubsub pattern

--- pubsub.js
+++ pubsub.js
@@ -2,24 +2,50 @@ const redis = require('redis');
 
 const CHANNELS = {
     TEST: 'TEST',
+    BLOCKCHAIN: 'BLOCKCHAIN'
 
 }
 
 class PubSub {
-    constructor() {
+    constructor({ blockchain }) {
+        this.blockchain = blockchain;
+
         this.publisher = redis.createClient();
         this.subscriber = redis.createClient();
 
-        this.subscriber.subscribe(CHANNELS.TEST);
+        this.subscribeToChannels();
 
         this.subscriber.on('message', (channel, message) => this.handleMessage(channel, message));
     }
 
+    subscribeToChannels() {
+        Object.values(CHANNELS).forEach(channel => {
+            this.subscriber.subscribe(channel);
+        });
+    }
+
     handleMessage(channel, message) {
         console.log(`Message received. Channel: ${channel}. Message: ${message}.`)
+
+        const parsedMessage = JSON.parse(message);
+
+        if (channel === CHANNELS.BLOCKCHAIN) {
+            this.blockchain.replaceChain(parsedMessage);
+        }
     }
+
+    publish({ channel, message }) {
+        this.publisher.publish(channel, message);
+    }
+
+    broadcastChain() {
+        this.publish({
+            channel: CHANNELS.BLOCKCHAIN,
+            message: JSON.stringify(this.blockchain.chain)
+        })
+    }
+
 }
 
-const testPubSub = new PubSub();
-
-setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
+module.exports = PubSub;
+// When one blockchain adds a new block, we want that blockchain node to publish its new version of the chain to the entire network.

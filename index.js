1514654428374669a7ccba3c70743ebc5001ac8a
Jere Sher <jeresher@g.ucla.edu>
2020-09-01 20:15:31 -0700

    temporarily merging index.js and routes.js due to conflict | added ability to sync on connect

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const express = require('express');
-const routes = require('./routes/routes');
-const { PORT } = require('./config/port');
+const request = require('request');
+const Blockchain = require('./services/blockchain');
+const PubSub = require('./network/pubsub');
+const { PORT, DEFAULT_PORT } = require('./config/port');
 const app = express();
 
-app.use(express.json());
-app.use(routes);
+const blockchain = new Blockchain();
+const pubsub = new PubSub( { blockchain });
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
-app.listen(PORT, () => console.log(`Listening at localhost: ${PORT}`))
+setTimeout(() => pubsub.broadcastChain(), 1000);
+
+app.use(express.json());
+
+app.get('/api/blocks', (req, res) => {
+    res.send(blockchain.chain);
+})
+
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body;
+
+    blockchain.addBlock({ data });
+
+    pubsub.broadcastChain();
+
+    res.redirect('/api/blocks');
+})
+
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (err, res, body) => {
+        if (!err && res.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+
+            console.log('replacing chain on sync with', rootChain)
+            blockchain.replaceChain(rootChain);
+        }
+    });
+}
+
+app.listen(PORT, () => {
+    console.log(`Listening at localhost: ${PORT}`)
+
+    syncChains();
+})

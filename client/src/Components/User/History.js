a5cc8cfa7c75968841f9d940e8a7b7b04425c075
Jere Sher <jeresher@g.ucla.edu>
2020-09-18 15:03:21 -0700

    created history skeelton

---
+++ client/src/Components/User/History.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function History() {
+    return (
+        <section className="history-container">
+        <h1>Your Transactions</h1>
+      </section>
+    )
+}
+
+export default History;

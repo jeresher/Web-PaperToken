288ca06b6f20a34d262ea0e1bff5d7d9c3c0ff6f
Jere Sher <jeresher@g.ucla.edu>
2020-09-20 23:09:53 -0700

    styled & added mine transaction button

--- client/src/Components/Miner/TransactionPool.js
+++ client/src/Components/Miner/TransactionPool.js
@@ -18,26 +18,42 @@ function TransactionPool() {
         .catch(err => console.log(err))
     }
 
+    function minePoolTransactions() {
+        fetch("http://localhost:5000/api/mine-transactions")
+        .then(res => window.location.reload())
+        .catch(err => console.log(err))
+    }
+
     useEffect(retrievePoolTransactions, [])
 
     return (
         <div className="transactionpool-container">
+
+            {/* POOL HEADING */}
             <h1>Transaction Pool</h1>
+
+            {/* POOL TABLE */}
             <div className="transactionpool-table">
-            <table>
-                <thead>
-                    <tr>
-                        <th className="transactionpool-address">Pending Transactions (Sender)</th>
-                        <th className="transactionpool-date">Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        {transactionPoolItems}
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th className="transactionpool-address">Pending Transactions (Sender)</th>
+                            <th className="transactionpool-date">Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {transactionPoolItems}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            {/* POOL MINE BUTTON */}
+            <div className="transactionpool-mine">
+                <button onClick={() => minePoolTransactions()}>Mine Transactions</button>
+            </div>
+
         </div>
     )
 }

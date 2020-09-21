1af89a458c2da2ffb0a0cb42083413963160e82f
Jere Sher <jeresher@g.ucla.edu>
2020-09-20 17:09:17 -0700

    added pagination (blocks) | page load starts at latest block

--- client/src/Components/Miner/Blockchain.js
+++ client/src/Components/Miner/Blockchain.js
@@ -4,7 +4,8 @@ import BlockchainItem from './BlockchainItem';
 function Blockchain() {
 
     const [blockchain, setBlockchain] = useState([]);
-    const [blockchainItems, setBlockchainItems] = useState([]);
+    const [blockchainItems, setBlockchainItems] = useState();
+    const [currentBlockIndex, setCurrentBlockIndex] = useState(0);
 
     function formatBlockchain(chain) {
         let formattedChain = [];
@@ -48,9 +49,7 @@ function Blockchain() {
     }
 
     useEffect(() => retrieveBlockchain(), [])
-    useEffect(() => {
-        console.log(blockchainItems)
-    }, [blockchainItems])
+    useEffect(() => setCurrentBlockIndex(blockchain.length-1), [blockchain])
 
     return (
         <div className="blockchain-container">
@@ -66,8 +65,9 @@ function Blockchain() {
                 </thead>
                 <tbody>
                     <tr>
-                        {blockchainItems.length > 0 ? blockchainItems[1] : null}
+                        {blockchainItems ? blockchainItems[currentBlockIndex] : null}
                     </tr>
+                    {/*
                     <tr>
                         <td className="blockchain-sender">04f9bb63e1304fcfe7b22d655e3c00f4fd27715b42a59be741a3f98e10963a145d82eb2cd57aff5e0a811c1cfee5c8c8225cb6875c4a799f2a5524329df0b34063</td>
                         <td className="blockchain-receiver">04f9bb63e1304fcfe7b22d655e3c00f4fd27715b42a59be741a3f98e10963a145d82eb2cd57aff5e0a811c1cfee5c8c8225cb6875c4a799f2a5524329df0b34063</td>
@@ -148,8 +148,37 @@ function Blockchain() {
                         <td className="blockchain-receiver">04f9bb63e1304fcfe7b22d655e3c00f4fd27715b42a59be741a3f98e10963a145d82eb2cd57aff5e0a811c1cfee5c8c8225cb6875c4a799f2a5524329df0b34063</td>
                         <td className="blockchain-amount">50000</td>
                     </tr>
+                    */}
                 </tbody>
             </table>
+            </div>
+            <div className="blockchain-navigation">
+
+                <button
+                    onClick={() => setCurrentBlockIndex(0)}
+                >{"<<"}</button>
+
+                <button
+                    onClick={(event) => {
+                        if (currentBlockIndex <= 0) setCurrentBlockIndex(0);
+                        else setCurrentBlockIndex(currentBlockIndex - 1);
+                    }}
+                >{"<"}</button>
+
+                <h6
+                    style={{display: 'inline', color:'white'}}
+                >{currentBlockIndex}</h6>
+
+                <button
+                    onClick={(event) => {
+                        if (currentBlockIndex >= blockchain.length-1) setCurrentBlockIndex(blockchain.length-1);
+                        else setCurrentBlockIndex(currentBlockIndex + 1);
+                    }}
+                >{">"}</button>
+
+                <button
+                    onClick={() => setCurrentBlockIndex(blockchain.length - 1)}
+                >{">>"}</button>
             </div>
         </div>
     )

5b53e56c4d8b0ff81f0476b208fc9963ac657664
Jere Sher <jeresher@g.ucla.edu>
2020-09-20 22:42:24 -0700

    Moved pagination into its own component

--- client/src/Components/Miner/Blockchain.js
+++ client/src/Components/Miner/Blockchain.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import BlockchainItem from './BlockchainItem';
+import BlockchainNavigation from './BlockchainNavigation';
 
 function Blockchain() {
 
-    const [blockchain, setBlockchain] = useState([]);
+    const [blockchain, setBlockchain] = useState([]); // blockchain = [[], [{sender, received, amount}, {...}], []]
     const [blockchainItems, setBlockchainItems] = useState();
     const [currentBlockIndex, setCurrentBlockIndex] = useState(0);
 
@@ -11,7 +12,7 @@ function Blockchain() {
         fetch("http://localhost:5000/api/blocks")
         .then(res => res.json())
         .then(result => {
-            let blockchain = formatBlockchain(result) // blockchain = [[], [{sender, received, amount}], []]
+            let blockchain = formatBlockchain(result) 
             setBlockchain(blockchain)
             setBlockchainItems(blockchain.map(block => block.map(transaction =>
                     <BlockchainItem transaction={transaction} />
@@ -48,88 +49,39 @@ function Blockchain() {
         return formattedChain;
     }
 
-    function configurePaginationButtons() {
-        let leftButtons = document.getElementsByClassName('left-buttons');
-        let rightButtons = document.getElementsByClassName('right-buttons');
-
-        function enableButtons(boolean, buttons) {
-
-            if (boolean === true) {
-                for (let i=0; i < buttons.length; i++) {
-                    buttons[i].style.color = "#5a5a5a";
-                    buttons[i].disabled = false;
-                }
-            } else {
-                for (let i=0; i < buttons.length; i++) {
-                    buttons[i].style.color = "#242424";
-                    buttons[i].disabled = true;
-                }
-            }
-
-        }
-
-        if (currentBlockIndex === 0) enableButtons(false, leftButtons);
-        else enableButtons(true, leftButtons);
-
-        if (currentBlockIndex === blockchain.length - 1) enableButtons(false, rightButtons);
-        else enableButtons(true, rightButtons);
-    }
-
-
-
     useEffect(() => retrieveBlockchain(), [])
     useEffect(() => setCurrentBlockIndex(blockchain.length-1), [blockchain])
-    useEffect(() => configurePaginationButtons(), [currentBlockIndex])
 
     return (
         <div className="blockchain-container">
+
+            {/* BLOCKCHAIN HEADING*/}
             <h1>Blockchain</h1>
+
+            {/* BLOCKCHAIN TABLE*/}
             <div className="blockchain-table">
-            <table>
-                <thead>
-                    <tr>
-                        <th className="blockchain-sender">Sender</th>
-                        <th className="blockchain-receiver">Receiver</th>
-                        <th className="blockchain-amount">Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        {blockchainItems ? blockchainItems[currentBlockIndex] : null}
-                    </tr>
-                </tbody>
-            </table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th className="blockchain-sender">Sender</th>
+                            <th className="blockchain-receiver">Receiver</th>
+                            <th className="blockchain-amount">Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {blockchainItems ? blockchainItems[currentBlockIndex] : null}
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            <div className="blockchain-navigation">
 
-                <button
-                    className="left-buttons"
-                    onClick={() => setCurrentBlockIndex(0)}
-                >{"<<"}</button>
-
-                <button
-                    className="left-buttons"
-                    onClick={(event) => {
-                        if (currentBlockIndex <= 0) setCurrentBlockIndex(0);
-                        else setCurrentBlockIndex(currentBlockIndex - 1);
-                    }}
-                >{"<"}</button>
-
-                <h6>Block {currentBlockIndex}</h6>
-
-                <button
-                    className="right-buttons"
-                    onClick={(event) => {
-                        if (currentBlockIndex >= blockchain.length-1) setCurrentBlockIndex(blockchain.length-1);
-                        else setCurrentBlockIndex(currentBlockIndex + 1);
-                    }}
-                >{">"}</button>
-
-                <button
-                    className="right-buttons"
-                    onClick={() => setCurrentBlockIndex(blockchain.length - 1)}
-                >{">>"}</button>
-            </div>
+            {/* BLOCKCHAIN NAVIGATION*/}
+            <BlockchainNavigation 
+                blockchain={blockchain} 
+                currentBlockIndex={currentBlockIndex}
+                setCurrentBlockIndex={setCurrentBlockIndex}
+            />
         </div>
     )
 }
---
+++ client/src/Components/Miner/BlockchainNavigation.js
@@ -0,0 +1,68 @@
+import React, {useEffect} from 'react';
+
+function BlockchainNavigation({blockchain, currentBlockIndex, setCurrentBlockIndex}) {
+
+    function configurePaginationButtons() {
+        let leftButtons = document.getElementsByClassName('left-buttons');
+        let rightButtons = document.getElementsByClassName('right-buttons');
+
+        function enableButtons(boolean, buttons) {
+
+            if (boolean === true) {
+                for (let i=0; i < buttons.length; i++) {
+                    buttons[i].style.color = "#5a5a5a";
+                    buttons[i].disabled = false;
+                }
+            } else {
+                for (let i=0; i < buttons.length; i++) {
+                    buttons[i].style.color = "#242424";
+                    buttons[i].disabled = true;
+                }
+            }
+
+        }
+
+        if (currentBlockIndex === 0) enableButtons(false, leftButtons);
+        else enableButtons(true, leftButtons);
+
+        if (currentBlockIndex === blockchain.length - 1) enableButtons(false, rightButtons);
+        else enableButtons(true, rightButtons);
+    }
+
+    useEffect(() => configurePaginationButtons(), [currentBlockIndex])
+
+    return (
+        <div className="blockchain-navigation">
+
+            <button
+                className="left-buttons"
+                onClick={() => setCurrentBlockIndex(0)}
+            >{"<<"}</button>
+
+            <button
+                className="left-buttons"
+                onClick={() => {
+                    if (currentBlockIndex <= 0) setCurrentBlockIndex(0);
+                    else setCurrentBlockIndex(currentBlockIndex - 1);
+                }}
+            >{"<"}</button>
+
+            <h6>Block {currentBlockIndex}</h6>
+
+            <button
+                className="right-buttons"
+                onClick={() => {
+                    if (currentBlockIndex >= blockchain.length-1) setCurrentBlockIndex(blockchain.length-1);
+                    else setCurrentBlockIndex(currentBlockIndex + 1);
+                }}
+            >{">"}</button>
+
+            <button
+                className="right-buttons"
+                onClick={() => setCurrentBlockIndex(blockchain.length - 1)}
+            >{">>"}</button>
+        </div>
+    )
+}
+
+export default BlockchainNavigation;

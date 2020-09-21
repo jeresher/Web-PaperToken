d14f6e281a2bd5d1889a30aca2418e3d5ebcb22c
Jere Sher <jeresher@g.ucla.edu>
2020-09-20 22:47:10 -0700

    enabled functionality of 'get Started' button on main page to navigate to wallet

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Background from "../Animation/Background"
-
+import { Link } from 'react-router-dom';
 
 function Home() {
 
@@ -13,7 +13,7 @@ function Home() {
     <div className="home-container">
       <h1>Paper<span>Token</span></h1>
       <h3>A full-fledged cryptocurrency and platform designed to <br /> showcase the innerworkings of blockchain technology.</h3>
-      <button>Get Started</button>
+      <Link to={'/user'}><button>Get Started</button></Link>
     </div>
   );
 }
